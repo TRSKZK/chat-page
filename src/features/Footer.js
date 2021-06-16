307863cf361f3f030586f9da58a9759749a1dfbe
TRSKZK <75508554+TRSKZK@users.noreply.github.com>
2021-06-16 17:33:32 +0300

    footer links refactored

--- src/features/Footer.js
+++ src/features/Footer.js
@@ -7,29 +7,38 @@ background-color:#1f1f20;
 width: 100%;
 height: 94px;
 
-`
-const LinksWrapp = styled.div`
-display:flex;
-justify-content:center;
-gap:10px;
 
 `
+const LinksWrapp = styled.ul`
+display:flex;
+justify-content:center;
+gap:25px;
+list-style: none;
+
+`
+
 const StyledLink = styled(Link)`
 color:white;
 font-size:24px;
 text-decoration: none;
 
 `
+
+const CopyRight = styled.p`
+text-align:center;
+color: white;
+margin: 0 0 10px 0;
+`
 export const Footer = () => {
     
-
     return (
         <FooterWrapp>
             <LinksWrapp>
-                <StyledLink to='/'>Home</StyledLink>
-                <StyledLink to='/contact'>Contact us</StyledLink>
-                <StyledLink to='/about'>About</StyledLink>
+               <li> <StyledLink to='/'>Home</StyledLink></li>
+               <li> <StyledLink to='/contact'>Contact us</StyledLink></li>
+               <li>  <StyledLink to='/about'>About</StyledLink></li>
             </LinksWrapp>
+            <CopyRight>@ Copyright 2019</CopyRight>
         </FooterWrapp>
     )
 }
